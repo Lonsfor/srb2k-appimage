5fe2cdc261b0c4d496ad3e788e36482dbe072db6
Lonsfor <Lonsfor@users.noreply.github.com>
2021-08-02 23:56:54 +0000

    Update dock.sh

--- dock.sh
+++ dock.sh
@@ -6,11 +6,10 @@ set -e
 cwd="$PWD"
 repo_root="$(readlink -f "$(dirname "$0")"/..)"
 
-image=srb2kappimage-build:"$DIST"-"$ARCH"
+image=srb2kappimage-build
 
 docker build -t "$image" -f "$repo_root"/Dockerfile"
 
 uid="$(id -u)"
 
-docker run --privileged --rm -i -e ARCH -e GITHUB_RUN_NUMBER -e CI=1 -v "$repo_root":/ws:ro -v "$cwd":/out "$image" \
-bash -xec 'cd /out && bash -xe /ws/build-everything.sh'
+docker run --privileged --rm -i -e ARCH -e GITHUB_RUN_NUMBER -e CI=1 -v "$repo_root":/ws:ro -v "$cwd":/out "$image" bash -xec 'cd /out && bash -xe /ws/build-everything.sh'
