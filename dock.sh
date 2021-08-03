6dc3cbe3faed9945f0c8185b2fe447cf7b10b756
Lonsfor <Lonsfor@users.noreply.github.com>
2021-08-03 00:02:42 +0000

    Update dock.sh

--- dock.sh
+++ dock.sh
@@ -8,8 +8,8 @@ repo_root="$(readlink -f "$(dirname "$0")"/..)"
 
 image=srb2kappimage-build
 
-docker build -t "$image" -f "$repo_root"/Dockerfile"
+docker build -t "$image" -f "$repo_root"/Dockerfile
 
 uid="$(id -u)"
 
-docker run --privileged --rm -i -v "$repo_root":/ws:ro -v "$cwd":/out "$image" bash -xec cd /out && bash -xe /ws/build-everything.sh
+docker run --privileged --rm -i -v "$repo_root":/ws:ro -v "$cwd":/out "$image" bash -xec 'cd /out && bash -xe /ws/build-everything.sh'
