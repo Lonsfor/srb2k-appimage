8d4967c8951ed47fcf770a16ec6840305370444e
Lonsfor <Lonsfor@users.noreply.github.com>
2021-08-06 23:45:37 -0400

    Switch to appimagetool
    Because linuxdeploy doesnt play well with appstream metadata

--- build-everything.sh
+++ build-everything.sh
@@ -25,38 +25,45 @@ LIBGME_CFLAGS= LIBGME_LDFLAGS=-lgme make -C src/ LINUX64=1 NOUPX=1 NOOBJDUMP=1 H
 cd ..
 
 wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget -q https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage
+chmod +x appimagetool-x86_64.AppImage
 
-mkdir -p AppDir/usr/bin
-mkdir -p AppDir/usr/share/applications
 install -Dm755 kart/bin/Linux64/Release/lsdl2srb2kart AppDir/usr/bin/srb2kart
 install -Dm755 AppRun AppDir/
 install -Dm644 kart/srb2.png AppDir/usr/share/icons/hicolor/256x256/apps/org.srb2.SRB2Kart.png
 install -Dm644 org.srb2.SRB2Kart.desktop AppDir/usr/share/applications/org.srb2.SRB2Kart.desktop
-chmod -R a+rx AppDir
+install -Dm644 org.srb2.SRB2Kart.appdata.xml AppDir/usr/share/metainfo/org.srb2.SRB2Kart.appdata.xml
 
-export VERSION=1.3-git-$(cd kart && git rev-parse --short HEAD)
+./linuxdeploy-x86_64.AppImage --appdir AppDir
 
-export UPDATE_INFORMATION="gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-noassets-x86_64.AppImage.zsync"
-OUTPUT=srb2kart-noassets-x86_64.AppImage ./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
+export NEWVERSION=1.3-git-$(cd kart && git rev-parse --short HEAD)
+export NEWCOMMITANDDATE=$(cd kart && git show --summary --pretty='format:"1.3-git-%h" date="%cs"')
+sed -i 's/VERSION/'"$NEWVERSION"'/g' AppDir/usr/share/applications/org.srb2.SRB2Kart.desktop
+sed -i 's/COMMITANDDATE/'"$NEWCOMMITANDDATE"'/g' AppDir/usr/share/metainfo/org.srb2.SRB2Kart.appdata.xml
+
+./appimagetool-x86_64.AppImage -n -u "gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-noassets-x86_64.AppImage.zsync" srb2kart-noassets-x86_64.AppImage
 
 wget -q https://github.com/STJr/Kart-Public/releases/download/v1.3/srb2kart-v13-Installer.exe
 7z x srb2kart-v13-Installer.exe -oAppDir/usr/games/SRB2Kart/ "*.kart" "*.srb" "mdls.dat" "mdls/*"
-chmod -R a+rx AppDir
 
-export UPDATE_INFORMATION="gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-x86_64.AppImage.zsync"
-OUTPUT=srb2kart-x86_64.AppImage ./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
+./appimagetool-x86_64.AppImage -n -u "gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-x86_64.AppImage.zsync" srb2kart-x86_64.AppImage
 
 mv AppDir/usr/games/ ./
 
 install -Dm755 moe/bin/Linux64/Release/lsdl2srb2kart AppDir/usr/bin/srb2kart
+install -Dm644 org.srb2.SRB2Kart.desktop AppDir/usr/share/applications/org.srb2.SRB2Kart.desktop
+install -Dm644 org.srb2.SRB2Kart.appdata.xml AppDir/usr/share/metainfo/org.srb2.SRB2Kart.appdata.xml
 
-export VERSION=1.3-moe-$(cd moe && git rev-parse --short HEAD)
+export NEWVERSION=1.3-moe-$(cd moe && git rev-parse --short HEAD)
+export NEWCOMMITANDDATE=$(cd moe && git show --summary --pretty='format:"1.3-moe-%h" date="%cs"')
+sed -i 's/VERSION/'"$NEWVERSION"'/g' AppDir/usr/share/applications/org.srb2.SRB2Kart.desktop
+sed -i 's/COMMITANDDATE/'"$NEWCOMMITANDDATE"'/g' AppDir/usr/share/metainfo/org.srb2.SRB2Kart.appdata.xml
 
-export UPDATE_INFORMATION="gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-moe-noassets-x86_64.AppImage.zsync"
-OUTPUT=srb2kart-moe-noassets-x86_64.AppImage ./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
+./appimagetool-x86_64.AppImage -n -u "gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-moe-noassets-x86_64.AppImage.zsync" srb2kart-moe-noassets-x86_64.AppImage
 
 mv ./games/ AppDir/usr/
 
 export UPDATE_INFORMATION="gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-moe-x86_64.AppImage.zsync"
 OUTPUT=srb2kart-moe-x86_64.AppImage ./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
+./appimagetool-x86_64.AppImage -n -u "gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-moe-x86_64.AppImage.zsync" srb2kart-moe-x86_64.AppImage
