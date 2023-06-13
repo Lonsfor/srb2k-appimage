f09a349a0839e1f18c00fcd353178c0273832832
Lonsfor <Lonsfor@users.noreply.github.com>
2023-06-13 01:06:13 -0400

    add HEP

--- build-everything.sh
+++ build-everything.sh
@@ -6,6 +6,7 @@ set -e
 sudo apt-get update -y -qq && sudo apt-get upgrade -y -qq
 sudo apt-get install -y -qq --no-install-recommends build-essential git nasm libpng-dev zlib1g-dev libsdl2-dev libsdl2-mixer-dev libgme-dev libopenmpt-dev libcurl4-openssl-dev rapidjson-dev cmake fuse3 pkg-config
 
+
 git clone --branch master --single-branch --no-tags https://github.com/discord/discord-rpc.git
 cd discord-rpc
 mkdir build
@@ -13,6 +14,7 @@ cd build
 cmake .. -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=/usr
 sudo cmake --build . --config Release --target install
 cd ../..
+
 
 git clone --branch master --single-branch --no-tags -n https://github.com/STJr/Kart-Public.git kart
 cd kart
@@ -28,10 +30,19 @@ patch src/d_netfil.c < ../d_netfil.c.patch
 LIBGME_CFLAGS= LIBGME_LDFLAGS=-lgme make -C src/ LINUX64=1 NOUPX=1 NOOBJDUMP=1 HAVE_DISCORDRPC=1
 cd ..
 
+git clone --branch Galaxy --single-branch --no-tags -n https://git.do.srb2.org/haya_/Kart-Public.git HEP
+cd HEP
+git checkout 4b8471c232f2024a679b546259d3b6dd943dc095
+patch src/d_netfil.c < ../d_netfil.c.patch
+LIBGME_CFLAGS= LIBGME_LDFLAGS=-lgme make -C src/ LINUX64=1 NOUPX=1 NOOBJDUMP=1 HAVE_DISCORDRPC=1
+cd ..
+
+
 wget -q https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 wget -q https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage
 chmod +x appimagetool-x86_64.AppImage
+
 
 install -Dm755 kart/bin/Linux64/Release/lsdl2srb2kart AppDir/usr/bin/srb2kart
 install -Dm755 AppRun AppDir/
@@ -50,11 +61,14 @@ sed -i 's/COMMITANDDATE/'"$NEWCOMMITANDDATE"'/g' AppDir/usr/share/metainfo/org.s
 ./appimagetool-x86_64.AppImage -n -u "gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-noassets-x86_64.AppImage.zsync" AppDir srb2kart-noassets-x86_64.AppImage
 
 wget -q https://github.com/STJr/Kart-Public/releases/download/v1.6/AssetsLinuxOnly.zip
-mkdir -p AppDir/usr/games/SRB2Kart
-unzip AssetsLinuxOnly.zip -d AppDir/usr/games/SRB2Kart *.kart *.srb mdls*
-chmod +w AppDir/usr/games/SRB2Kart/mdls
+mkdir -p AppDir/usr
+mkdir -p games/SRB2Kart
+unzip AssetsLinuxOnly.zip -d games/SRB2Kart *.kart *.srb mdls*
+chmod +w games/SRB2Kart/mdls
+cp -r games AppDir/usr
 
 ./appimagetool-x86_64.AppImage -n -u "gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-x86_64.AppImage.zsync" AppDir srb2kart-x86_64.AppImage
+
 
 install -Dm755 galaxy/bin/Linux64/Release/lsdl2srb2kart AppDir/usr/bin/srb2kart
 install -Dm644 org.srb2.SRB2Kart.desktop AppDir/usr/share/applications/org.srb2.SRB2Kart.desktop
@@ -70,3 +84,20 @@ sed -i 's/COMMITANDDATE/'"$NEWCOMMITANDDATE"'/g' AppDir/usr/share/metainfo/org.s
 rm -rf AppDir/usr/games
 
 ./appimagetool-x86_64.AppImage -n -u "gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-galaxy-noassets-x86_64.AppImage.zsync" AppDir srb2kart-galaxy-noassets-x86_64.AppImage
+
+
+install -Dm755 HEP/bin/Linux64/Release/lsdl2srb2kart AppDir/usr/bin/srb2kart
+install -Dm644 org.srb2.SRB2Kart.desktop AppDir/usr/share/applications/org.srb2.SRB2Kart.desktop
+install -Dm644 org.srb2.SRB2Kart.appdata.xml AppDir/usr/share/metainfo/org.srb2.SRB2Kart.appdata.xml
+
+export NEWVERSION=1.6-HEP-$(cd galaxy && git rev-parse --short HEAD)
+export NEWCOMMITANDDATE=$(cd HEP && git show --summary --pretty='format:"1.6-HEP-%h" date="%cs"')
+sed -i 's/VERSION/'"$NEWVERSION"'/g' AppDir/usr/share/applications/org.srb2.SRB2Kart.desktop
+sed -i 's/COMMITANDDATE/'"$NEWCOMMITANDDATE"'/g' AppDir/usr/share/metainfo/org.srb2.SRB2Kart.appdata.xml
+
+./appimagetool-x86_64.AppImage -n -u "gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-HEP-noassets-x86_64.AppImage.zsync" AppDir srb2kart-HEP-noassets-x86_64.AppImage
+
+cp -r games AppDir/usr
+wget -O AppDir/usr/games/SRB2Kart/snowy_files.kart "https://drive.google.com/uc?export=download&id=1luvNekal7yFMAvwa7sTywaEB1akHFdP3"
+
+./appimagetool-x86_64.AppImage -n -u "gh-releases-zsync|lonsfor|srb2k-appimage|latest|srb2kart-HEP-x86_64.AppImage.zsync" AppDir srb2kart-HEP-x86_64.AppImage
